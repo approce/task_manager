1cb0ac406e71b480b65c98d61d6bf7c0ff93b1b7
Roman Isko <iskoroman@gmail.com>
2016-01-28 17:37:43 +0200

    add listen port

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users  = require('./routes/users');
 
 var app = express();
 
+app.listen(3000);
 app.use(express.static(__dirname + '/public'));
 
 // uncomment after placing your favicon in /public
